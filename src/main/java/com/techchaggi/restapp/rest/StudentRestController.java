69fa8887320474768b7d6ac9b34414b2d7297356
a-kumar5 <ayush.1306.kumar@gmail.com>
2023-08-28 20:54:41 +0530

    used @PostConstruct to refactor the code to load student data once.

--- src/main/java/com/techchaggi/restapp/rest/StudentRestController.java
+++ src/main/java/com/techchaggi/restapp/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.techchaggi.restapp.rest;
 
 import com.techchaggi.restapp.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Bruce", "Wayne"));
+        theStudents.add(new Student("Clark", "Kent"));
+        theStudents.add(new Student("Alfred", "Penneyworth"));
+    }
+
     // define a endpoint for "/students" - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("Bruce", "Wayne"));
-        theStudents.add(new Student("Clark", "Kent"));
-        theStudents.add(new Student("Alfred", "Penneyworth"));
+
         return theStudents;
     }
-
 }
